f55455464625ce7f86497be1054d46979b672b70
yasinshamim <ashkanrazavi371@gmail.com>
2026-07-20 03:48:40 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+document.getElementById("enter").onclick = () => {
 
+const pass = document.getElementById("password").value;
+
+if(pass=="1130" || pass=="66"){
+
+alert("در نسخه بعدی وارد فضا میشی 🚀❤️");
+
+}else{
+
+alert("رمز اشتباهه 🥺");
+
+}
+
+}
